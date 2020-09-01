d404f8d971e0f6264523504d463692c92841e539
seemywingz <kevin.jayne@icloud.com>
2020-09-01 13:35:31 -0400

    ignore node_modules

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@
 import * as tb from '3base';
 import Boxes from './Boxes';
 
-let manager = new tb.Manager({canvas: myCanvas, antialias: true});
+let manager = new tb.Manager({"onLoad": ()=>{
+    document.getElementById( 'loading-screen' ).classList.add( 'fade-out' );
+  }});
 manager.loadScene(Boxes);
